57bf31959038861b9480976c287717e09da4e7ee
EuMotta <hyperxmotta@hotmail.com>
2023-02-01 21:28:09 -0300

    logo and navbar updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,29 +1,35 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { navLinks } from '../constants';
+import logo from '../public/Logo4.svg';
 
 const Navbar = () => (
-  <div>
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
-      <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white ">
-            {navLinks.map((link) => (
-              <li>
-                <Link
-                  href={link.href}
-                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <div className="container flex flex-wrap items-center justify-between mx-auto ">
+    <div className="md:block md:w-1/3">
+      <Image src={logo} width={100} height={100} />
+    </div>
+    <div className="md:block md:w-1/3 text-center">
+      <ul className="flex p-4 mt-4 border border-gray-100  md:flex-row justify-center items-center md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
+        {navLinks.map((link) => (
+          <li>
+            <Link
+              href={link.href}
+              className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+            >
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className="md:block md:w-1/3 text-right">
+      <button type="button" className="bg-blue-700 text-white py-2 px-4 rounded">
+        Login
+      </button>
+    </div>
   </div>
 );
 
