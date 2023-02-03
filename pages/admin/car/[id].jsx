19ce7cedefd98c31591794b598921a1af7d294e1
EuMotta <hyperxmotta@hotmail.com>
2023-02-03 09:36:34 -0300

    car card function complete + car id screen

---
+++ pages/admin/car/[id].jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AdminCarScreen = () => <div>car</div>;
+
+export default AdminCarScreen;
--- sections/showCars/ShowCars.jsx
+++ sections/showCars/ShowCars.jsx
@@ -20,47 +20,38 @@ const ShowCars = () => {
   return (
     <div className={styles.xPaddings}>
       <div className="grid grid-cols-4">
-        {carsData.slice(1, 2).map((car, index) => {
-          const image = car.image.toString('base64');
-          console.log('car.image:', car.image);
-          return (
-            <div key={index} className="glassmorphism">
-              <div className="text-center">
-                <div className="font-bold">{car.owner}</div>
-              </div>
-              <Image
-                src={`data:image/jpg;base64,${image}`}
-                width={300}
-                height={300}
-                alt="img"
-              />
-              <div className="text-center font-bold bg-gray-50 shadow-sm shadow-slate-300">
-                {car.model}
-              </div>
-              <div className="p-2">
-                <div>
-                  <span className="font-semibold">Dono:</span> {car.Ownername}
-                </div>
-                <div>
-                  <span className="font-semibold">Fabricante:</span>{' '}
-                  {car.manufacturer}
-                </div>
-                <div>
-                  <span className="font-semibold">Ano:</span> {car.year}
-                </div>
-                <div>
-                  <span className="font-semibold">Combustivel:</span> {car.fuel}
-                </div>
-              </div>
-              <Link
-                href="/"
-                className="bg-gray-300 hover:bg-gray-500 hover:text-white flex justify-center"
-              >
-                Ver mais
-              </Link>
+        {carsData.slice(1, 2).map((car, index) => (
+          <div key={index} className="glassmorphism">
+            <div className="text-center">
+              <div className="font-bold">{car.owner}</div>
             </div>
-          );
-        })}
+            <Image src={car.image} width={300} height={300} alt="img" />
+            <div className="text-center font-bold bg-gray-50 shadow-sm shadow-slate-300">
+              {car.model}
+            </div>
+            <div className="p-2">
+              <div>
+                <span className="font-semibold">Dono:</span> {car.Ownername}
+              </div>
+              <div>
+                <span className="font-semibold">Fabricante:</span>{' '}
+                {car.manufacturer}
+              </div>
+              <div>
+                <span className="font-semibold">Ano:</span> {car.year}
+              </div>
+              <div>
+                <span className="font-semibold">Combustivel:</span> {car.fuel}
+              </div>
+            </div>
+            <Link
+              href={`/admin/car/${car._id}`}
+              className="bg-gray-300 hover:bg-gray-500 hover:text-white flex justify-center"
+            >
+              Ver mais
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
